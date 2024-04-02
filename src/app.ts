b1ef6cb1abf7af89e225f62db38c95301401e5f1
igalshilman <igal@restate.dev>
2024-04-02 15:08:37 +0200

    Add few more example handlers

--- src/app.ts
+++ src/app.ts
@@ -13,10 +13,101 @@ import * as restate from "@restatedev/restate-sdk";
 // Have a look at the TS docs on the context, or at https://docs.restate.dev/
 //
 
+
+
 const myService = restate.router({
-  hello: async (ctx: restate.Context, name: string) => {
-  return `Hello ${name}!`;
-}
+
+  /**
+   * Hello handler - a simple hello world restate handler.
+   *  
+   * Try it out (check out the bottom of this file how to start restate):
+   * 
+   * curl -X POST -H 'content-type: application/json' http://localhost:8080/myservice/hello -d '{ "request": { "name" : "Bob" } }'
+   * 
+   * 
+   * @param ctx restate context. 
+   * @param request the contents of the request's payload
+   * @returns a personalized, durable, greeting.
+   */
+  hello: async (ctx: restate.Context, request: {name: string}) => {
+    return `Hello ${request.name}!`;
+  },
+
+  /**
+   * FirstProductInCart - this handler demonstrates how to use sideEffects. 
+   * 
+   * This handler fetches the content of a shopping cart with a given cartId,
+   * and then fetches the first product's description that was in that cart.
+   * 
+   * Try it out (check out the bottom of this file how to start restate):
+   * 
+   * curl -X POST -H 'content-type: application/json' http://localhost:8080/myservice/firstProductInCart -d '{ "request": { "cartId": "1"} }'
+   * 
+   * @param ctx restate context. 
+   * @param request 
+   * @returns a description of the first product in the cart
+   */
+  firstProductInCart: async (ctx: restate.Context, request: { cartId: string }) => {
+
+    const cart = await ctx.sideEffect(async () => {
+      //
+      // fetch the cart contents within the sideEffect.
+      //
+      const res = await fetch(`https://dummyjson.com/carts/${request.cartId}`);
+      return (await res.json()) as {
+        products: Array<{id: string}>
+      };
+    });
+
+    //
+    // At this point, we can use `cart` reliably, even if at this point our handler will crash,
+    // Restate had journald the fact that this sideEffect was previously successful with the result durably stored.
+    //
+
+    //
+    // now let's fetch the description of the first item in the cart.
+    //
+    const firstProduct = cart.products.pop();
+    if (!firstProduct) {
+      return {};
+    }
+
+    const product = await ctx.sideEffect(async () => {
+      //
+      // fetch the product description within the sideEffect.
+      //
+      const res = await fetch(`https://dummyjson.com/products/${firstProduct.id}`);
+      return (await res.json()) as {
+        id: string;
+        description: string;
+        thumbnail: string;
+      };
+    });
+   
+    return {description: product.description}
+  },
+
+  /**
+   * SleepyHandler - demonstrates how to use ctx.sleep
+   * 
+   * Try it out (check out the bottom of this file how to start restate):
+   * 
+   * curl -X POST -H 'content-type: application/json' http://localhost:8080/myservice/sleepyHandler -d '{ "request": { "duration": 1000, "times" : 3} }'
+   * 
+   * 
+   * @param ctx the restate context 
+   * @param request how many times to sleep and for how long to sleep. 
+   */
+  sleepyHandler: async (ctx: restate.Context, request: { duration: number, times: number }) => {
+
+    for (let i = 0 ; i < request.times ; i++) {
+      ctx.console.log(`About to sleep at the ${i}-th time. Zzz....`);
+      await ctx.sleep(request.duration);
+      ctx.console.log(`Done sleeping!`);
+    }
+
+    return `slept for a total of ${ request.duration * request.times } milliseconds`
+  },
 
 });
 
@@ -25,17 +116,13 @@ restate
   .endpoint()
   .bindRouter(
     "myservice", // the name of the service that serves the handlers
-     myService // the routes and handlers in the service
+    myService // the routes and handlers in the service
   )
   .listen(9080);
 
 // --------------
 //  Testing this
 // --------------
-//
-// Invoke this by calling Restate to invoke this handler durably:
-//
-//    curl -X POST -H 'content-type: application/json' http://localhost:8080/myservice/hello -d '{ "request": "Friend" }'
 //
 // To launch Restate and register this service (if you don't have Restate running already)
 //
@@ -46,3 +133,4 @@ restate
 //  - On Linux:
 //    docker run --name restate_dev --rm --network=host docker.io/restatedev/restate:latest
 //    curl -X POST http://localhost:9070/deployments -H 'content-type: application/json' -d '{"uri": "http://localhost:9080"}'
+//
